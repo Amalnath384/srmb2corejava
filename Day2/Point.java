9035689b165750e5410aa81119aa6c36643603d1
Amalnath384 <86220114+Amalnath384@users.noreply.github.com>
2021-06-24 12:52:36 +0530

    Add files via upload

--- Day2/Point.java
+++ Day2/Point.java
@@ -1,13 +1,14 @@
+ import java.lang.*;
  class Point {
 	private int x;
 	private int y;
 	Point(){
-		System.out.println("Hello");
+		
 	}
 	 Point(int x,int y){
 		this.x=x;
 		this.y=y;
-		System.out.println(x+" "+y);
+		
 	}
 	public void setX(int x){
 		this.x=x;
@@ -19,25 +20,28 @@
 		this.x=x;
 		this.y=y;
 	}
-	public void distance(Point p1,Point p2){
-		new Point();
-		new Point();
-		p1.x=30;
-		p2.x=40;
-		System.out.println(p1.x+" "+p2.x);
+	public double distance(Point p2){
+	return (Math.sqrt( (x-p2.x)*(x-p2.x)+(y-p2.y)*(y-p2.y)));
+	
 		
 	}
 	
 	public static void main(String[] args) {
-		Point ref=new Point(2,5);
+		double a, b, c;
+       
+		Point ref=new Point(6,6);
 		Point obj=new Point();
 		Point obj1=new Point();
-		
-		ref.setX(1);
-		ref.setY(2);
-		ref.setXY(3, 4);
-		ref.distance(obj, obj1);
-			
+		ref.setX(6);
+		ref.setY(6);
+		ref.setXY(6, 6);
+		a = ref.distance(obj);
+        b = obj.distance(obj1);
+        c = ref.distance(obj1);
+        System.out.println("Distance between point p1 to p2 is " + a);
+        System.out.println("Distance between point p2 to p is "  + b);
+        System.out.println("Distance between point p to p1 is "  + c);
+        	
 		
 	}
 
