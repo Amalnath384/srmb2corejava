371379391f71bdb4e8ae2ce7fa33625fcba92a35
Amalnath384 <86220114+Amalnath384@users.noreply.github.com>
2021-07-01 11:51:57 +0530

    Add files via upload

---
+++ Day9/Employee.java
@@ -0,0 +1,37 @@
+package Day9;
+
+import java.util.LinkedList;
+
+class details{
+	private String eno;
+	private String name;
+	private String desi;
+	
+	details(String eno,String name,String desi){
+		this.eno=eno;
+		this.name=name;
+		this.desi=desi;
+	}
+	public String toString(){
+		return("Employee no: "+eno+" "+"Employee name: "+name+" "+"Designation: "+desi);
+	}
+	
+}
+public class Employee {
+
+	public static void main(String[] args) {
+		
+		LinkedList<details> ref=new LinkedList<details>();
+		ref.add(new details("A121","Amal","Dev"));
+		ref.add(new details("A122","Ashok","Dev"));
+		ref.add(new details("A123","Arun","Tes"));
+		
+		for(details det:ref){
+			System.out.println(det);
+		}
+		
+
+	}
+
+}
+
